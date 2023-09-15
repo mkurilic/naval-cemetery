a4c78041dc7dd6ae9633dbb2a505798e6acfce52
mkurilic <mkurilic@unipu.hr>
2023-09-15 05:35:04 +0200

    Pushing Marzipano project files

---
+++ data.js
@@ -0,0 +1,507 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ulaz",
+      "name": "Ulaz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.47914225148075396,
+        "pitch": 0.11182132118766397,
+        "fov": 1.4479086935421337
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7221717452108063,
+          "pitch": 0.28821126869488545,
+          "rotation": 0,
+          "target": "1-groblje-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.05611457290619448,
+          "pitch": -0.21838567471568027,
+          "title": "Mornaričko spomen groblje<br>",
+          "text": "Mornaričko spomen groblje u Puli (njem. K.u.k. Marinefriedhof) nastalo je 1862. za potrebe sahranjivanja vojnih osoba.<br><br>27. listopada 1960. Mornaričko je groblje proglašeno za spomen groblje te se od tada ondje više nisu vršili ukopi. Do tada je ondje sahranjeno oko 150.000 ljudi.<br><br>Groblje je renovirano u periodu 1989. - 1991, od strane austrijskog Crnog Križa te skupštine općine Pula u suradnji s Njemačkim savezom za brigu o ratnim grobljima."
+        }
+      ]
+    },
+    {
+      "id": "1-groblje-1",
+      "name": "Groblje-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.08627581778115356,
+        "pitch": 0.15137218865520552,
+        "fov": 1.4479086935421337
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1358429085680397,
+          "pitch": 0.5252731931298396,
+          "rotation": 0,
+          "target": "0-ulaz"
+        },
+        {
+          "yaw": -0.23641385569389506,
+          "pitch": 0.4754802491039545,
+          "rotation": 12.566370614359176,
+          "target": "2-groblje-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-groblje-2",
+      "name": "Groblje-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -1.6284975477458516,
+        "pitch": 0.12731060967140273,
+        "fov": 1.4479086935421337
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5945986518981279,
+          "pitch": 0.4595037790339358,
+          "rotation": 0,
+          "target": "1-groblje-1"
+        },
+        {
+          "yaw": -1.986475771728733,
+          "pitch": 0.15051943120234057,
+          "rotation": 0,
+          "target": "3-groblje-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-groblje-3",
+      "name": "Groblje-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.41379855721410763,
+        "pitch": 0.18664765025509666,
+        "fov": 1.4479086935421337
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.31063578051119123,
+          "pitch": 0.4567142370962838,
+          "rotation": 0,
+          "target": "4-groblje-4"
+        },
+        {
+          "yaw": 2.5643536424453544,
+          "pitch": 0.3939356180535558,
+          "rotation": 0,
+          "target": "2-groblje-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-groblje-4",
+      "name": "Groblje-4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.6127594959672855,
+        "pitch": 0.19065847436172945,
+        "fov": 1.4479086935421337
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6463755798064055,
+          "pitch": 0.12619359822971,
+          "rotation": 0,
+          "target": "5-groblje-5"
+        },
+        {
+          "yaw": 0.4666111657106864,
+          "pitch": 0.3968793639950672,
+          "rotation": 5.497787143782138,
+          "target": "3-groblje-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5412373561716173,
+          "pitch": -0.33714243605473726,
+          "title": "U spomen Janku Vukoviću Podkapelskom<br>",
+          "text": "Zapovjednik bojnog broda Viribus Unitis i hrvatski admiral koji je 1.11.1918. poginuo u pulskoj luci.<br>"
+        }
+      ]
+    },
+    {
+      "id": "5-groblje-5",
+      "name": "Groblje-5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -1.7982541391957945,
+        "pitch": 0.16085128979737284,
+        "fov": 1.4479086935421337
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.839600254251895,
+          "pitch": 0.21429309320854806,
+          "rotation": 0,
+          "target": "6-groblje-6"
+        },
+        {
+          "yaw": 1.2651841945146725,
+          "pitch": 0.37515826125468443,
+          "rotation": 0,
+          "target": "4-groblje-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-groblje-6",
+      "name": "Groblje-6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.4643428525906774,
+        "pitch": 0.10145278048534045,
+        "fov": 1.4479086935421337
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.47181135467501,
+          "pitch": 0.2558424806293207,
+          "rotation": 0,
+          "target": "7-groblje-7"
+        },
+        {
+          "yaw": -0.7137036152918572,
+          "pitch": 0.6060979868800693,
+          "rotation": 0,
+          "target": "5-groblje-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4766217727725426,
+          "pitch": 0.08577154686040345,
+          "title": "Spomenik supružnicima Cornelie i Georg Huebert<br>",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "7-groblje-7",
+      "name": "Groblje-7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.6921238262101852,
+        "pitch": 0.07530871005933193,
+        "fov": 1.4479086935421337
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3258383291768858,
+          "pitch": 0.3898826567031328,
+          "rotation": 0,
+          "target": "8-groblje-8"
+        },
+        {
+          "yaw": -1.5907108634850076,
+          "pitch": 0.32451156813894677,
+          "rotation": 0,
+          "target": "6-groblje-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-groblje-8",
+      "name": "Groblje-8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 3.011358127767739,
+        "pitch": 0.33661336156364996,
+        "fov": 1.4479086935421337
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9949333836240815,
+          "pitch": 0.1957828739803631,
+          "rotation": 7.853981633974483,
+          "target": "9-groblje-9"
+        },
+        {
+          "yaw": -0.13268304897753502,
+          "pitch": 0.14453877589079234,
+          "rotation": 0,
+          "target": "7-groblje-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-groblje-9",
+      "name": "Groblje-9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -3.0644726457981477,
+        "pitch": 0.5493485731065846,
+        "fov": 1.4479086935421337
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8482186086620409,
+          "pitch": -0.19175010730024056,
+          "rotation": 5.497787143782138,
+          "target": "8-groblje-8"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4658689599071906,
+          "pitch": 0.06846188674971465,
+          "title": "Spomenik Matilde Barry<br>",
+          "text": "<div>Govori se da se često njen lik u crnini ukazuje na groblju, najčešće oko 13. siječnja. Njezin grob uvijek djeluje održavano te zaštitari često ondje nađu zapaljenu svijeću i cvijeće, iako kažu kako nikad nisu vidjeli da ih netko donosi.&nbsp;</div><div><br></div><div>Tako postoji vjerovanje kako Matilda, iako je umrla prije 150 godina, samostalno održava svoj grob.</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
